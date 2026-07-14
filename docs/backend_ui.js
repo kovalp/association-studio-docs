dff6715a16522578e0af9a29bb53582f68063bad
Peter Koval <pkoval@vicomtech.org>
2026-07-14 22:12:43 +0200

    + rotating cursor

--- docs/backend_ui.js
+++ docs/backend_ui.js
@@ -2,6 +2,19 @@ import {BboxHelper} from "./bbox_helper.js";
 import {BboxPlot} from "./bbox_plot.js";
 import {Screen} from "./screen.js";
 
+
+const rotate_cursor = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='black' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'><path d='M21.5 2v6h-6M21.34 15.57a10 10 0 1 1-.57-8.38l5.67-5.67'/></svg>") 12 12, auto`;
+
+function get_cursor(is_in_box, is_in_moon){
+    if (is_in_box) {
+        return "pointer";
+    } else if (is_in_moon) {
+        return rotate_cursor;
+    }
+    return "";
+}
+
+
 class BackendUi {
     constructor(canvas_id) {
         this.canvas = document.getElementById(canvas_id);
@@ -11,6 +24,8 @@ class BackendUi {
         this.box_hlp = new BboxHelper([5.0, 6.0, 0.5, 3.0, 1.5]);
         this.box_plt.draw(this.ctx, this.box_hlp);
         this.is_dragging = false;
+        this.is_in_box = false;
+        this.is_in_moon = false;
         this.start_event = new MouseEvent("", undefined);
         this.canvas.addEventListener('mousedown', this.mouse_down_callback.bind(this));
         this.canvas.addEventListener('mousemove', this.mouse_move_callback.bind(this));
@@ -33,18 +48,27 @@ class BackendUi {
     }
 
     mouse_down_callback(event){
-        if (this.box_hlp.is_in(event.offsetX, event.offsetY)) {
+        this._upd_in_flags(event);
+        if (this.is_in_box || this.is_in_moon) {
             this.is_dragging = true;
             this.start_event = event;
         }
     }
 
     mouse_move_callback(event){
-        event.target.style.cursor = this.box_hlp.is_in(event.offsetX, event.offsetY) ? 'pointer': '';
-        if (this.is_dragging){
+        if (this.is_dragging) {
             let box_cpy = new BboxHelper(this.box_hlp.xy_yaw_wh);
             this.translate_and_draw(box_cpy, event);
+        } else {
+            this._upd_in_flags(event);
+            event.target.style.cursor = get_cursor(this.is_in_box, this.is_in_moon);
         }
+    }
+
+    _upd_in_flags(event){
+        this.box_hlp.set_screen_offset(event.offsetX, event.offsetY);
+        this.is_in_box = this.box_hlp.is_in_box();
+        this.is_in_moon = this.box_hlp.is_in_moon();
     }
 }
 
--- docs/bbox_helper.js
+++ docs/bbox_helper.js
@@ -2,6 +2,7 @@ class BboxHelper {
     constructor(xy_yaw_wh) {
         this.xy_yaw_wh = new Float32Array(xy_yaw_wh);
         this.transform_scale = 20;
+        this.moon_radius = 0.5;
         this.transform = new DOMMatrix();
         this.transform.scaleSelf(this.transform_scale, this.transform_scale);
         this.transform.translateSelf(xy_yaw_wh[0], xy_yaw_wh[1]);
@@ -9,6 +10,7 @@ class BboxHelper {
         this.inv_transform = this.transform.inverse();
         this.half_sxy = new Float32Array([xy_yaw_wh[3] / 2, xy_yaw_wh[4] / 2]);
         this.offset_xy = new DOMPoint(0, 0);
+        this.data_xy = new DOMPoint(0, 0);
     }
 
     scale(num){
@@ -25,12 +27,22 @@ class BboxHelper {
         this.inv_transform = this.transform.inverse();
     }
 
-    is_in(offset_x, offset_y) {
-        this.offset_xy.x = offset_x;
-        this.offset_xy.y = offset_y;
-        let xy = this.inv_transform.transformPoint(this.offset_xy);
+    set_screen_offset(offset_x, offset_y) {
+        Object.assign(this.offset_xy, { x: offset_x, y: offset_y });
+        this.data_xy = this.inv_transform.transformPoint(this.offset_xy);
+    }
+
+    is_in_box() {
+        let xy = this.data_xy;
         return (Math.abs(xy.x) < this.half_sxy[0]) && (Math.abs(xy.y) < this.half_sxy[1]);
     }
+
+    is_in_moon() {
+        let dx = this.data_xy.x - this.half_sxy[0];
+        let r = Math.sqrt(dx ** 2 + this.data_xy.y ** 2);
+        return r < this.moon_radius && dx > 0.;
+    }
+
 }
 
 export { BboxHelper };
