75fe6523ea4afa19f98a69cf78efb412604fa38e
Peter Koval <pkoval@vicomtech.org>
2026-07-15 17:27:59 +0200

    little refactoring in the bbox_helper

--- docs/bbox_helper.js
+++ docs/bbox_helper.js
@@ -7,13 +7,18 @@ class BboxHelper {
         this.transform_scale = 20;
         this.moon_radius = 0.5;
         this.transform = new DOMMatrix();
-        this.transform.scaleSelf(this.transform_scale, this.transform_scale);
-        this.transform.translateSelf(xy_yaw_wh[0], xy_yaw_wh[1]);
-        this.transform.rotateSelf(180 * xy_yaw_wh[2] / Math.PI);
-        this.inv_transform = this.transform.inverse();
+        this._upd_transform();
         this.half_sxy = new Float32Array([xy_yaw_wh[3] / 2, xy_yaw_wh[4] / 2]);
         this.offset_xy = new DOMPoint(0, 0);
         this.data_xy = new DOMPoint(0, 0);
+    }
+
+    _upd_transform() {
+        this.transform.setMatrixValue('');
+        this.transform.scaleSelf(this.transform_scale, this.transform_scale);
+        this.transform.translateSelf(this.xy_yaw_wh[0], this.xy_yaw_wh[1]);
+        this.transform.rotateSelf(180 * this.xy_yaw_wh[2] / Math.PI);
+        this.inv_transform = this.transform.inverse();
     }
 
     scale(num){
@@ -23,22 +28,13 @@ class BboxHelper {
     translate(dx, dy) {
         this.xy_yaw_wh[0] += dx;
         this.xy_yaw_wh[1] += dy;
-        this.transform.setMatrixValue('');
-        this.transform.scaleSelf(this.transform_scale, this.transform_scale);
-        this.transform.translateSelf(this.xy_yaw_wh[0], this.xy_yaw_wh[1]);
-        this.transform.rotateSelf(180 * this.xy_yaw_wh[2] / Math.PI);
-        this.inv_transform = this.transform.inverse();
+        this._upd_transform();
     }
 
     rotate(angle_rad) {
         this.xy_yaw_wh[2] += angle_rad;
         this.xy_yaw_wh[2] = ((this.xy_yaw_wh[2] % two_pi) + two_pi) % two_pi;
-        this.transform.setMatrixValue('');
-        this.transform.scaleSelf(this.transform_scale, this.transform_scale);
-        this.transform.translateSelf(this.xy_yaw_wh[0], this.xy_yaw_wh[1]);
-        this.transform.rotateSelf(180 * this.xy_yaw_wh[2] / Math.PI);
-        this.transform.rotateFromVectorSelf()
-        this.inv_transform = this.transform.inverse();
+        this._upd_transform();
     }
 
     set_screen_offset(offset_x, offset_y) {
@@ -56,7 +52,6 @@ class BboxHelper {
         let r = Math.sqrt(dx ** 2 + this.data_xy.y ** 2);
         return r < this.moon_radius && dx > 0.;
     }
-
 }
 
 export { BboxHelper };
